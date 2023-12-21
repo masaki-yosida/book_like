6d7eeba7fef70b6557246eca7e491b52b292b05c
masaki-yosida <yashukasono@gmail.com>
2023-12-21 15:04:45 +0900

    本の新規登録できちゃう

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,26 @@
+# app/controllers/books_controller.rb
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to books_path, notice: 'Book was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :description)
+  end
+end
